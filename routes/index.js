546f14f51286f14e0e8465a81d9f420079455606
thomas <thomasgrz@outlook.com>
2018-11-11 20:20:50 -0500

    type:Style
    reworked the padding on .container
    
    :

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LeagueSide Sponsor Locator' });
+  res.render('index', { title: 'SportSponsor Locator' });
 });
 
 module.exports = router;
